02a21c2c056642dc164271f51b040676eb378b64
Taylor Thornton <thornrae@gmail.com>
2020-03-28 19:19:51 -0700

    lab 3 additions

--- js/app.js
+++ js/app.js
@@ -1,67 +1,104 @@
 'use strict';
 
+var correctAnswer = 0;
 var userName = prompt('What is your name');
-console.log(userName + 'this is the answer for userName prompt');
+// console.log(userName + 'this is the answer for userName prompt');
 
-
-var read = prompt('Do you like to read?');
-var read = read.toUpperCase();
-var read = read.toLowerCase();
+// Q1-Do I like to read question
+var read = prompt(userName + ', do I like to read?').toLowerCase();
 // console.log(read + 'this is answer for read prompt');
 if(read === 'yes'){
-  alert('That\'s awesome ' + userName + ' me too! Right now I\'m reading Over Story by Richard Powers.');
+  alert('Correct, ' + userName + '! I like to read.');
+  correctAnswer ++;
 } else if(read === 'no'){
-  alert(userName + ' now might be a good time to pick up a book! I recommend Overstory by Richard Powers.');
-} else{
-  alert(userName + ' ...um hello, I asked if you read books!');
+  alert('Wrong, ' + userName + '. I do like to read.');
+} else {
+  prompt('One more time ' + userName + '! Do I like to read?');
 }
 
-var netflix = prompt('Do you have a favorite Netflix show?');
-var netflix = netflix.toUpperCase();
-var netflix = netflix.toLowerCase();
+// Q2-Netflex Question
+var netflix = prompt(userName + ', do I watch Netflix?').toLowerCase();
 // console.log(netflix + 'this is answer for netflix prompt');
-if(read === 'yes'){
-  alert('Dontcha just love Netflix, ' + userName + '?! My favorite show on Netflix is Schitts Creek!' );
-} else if(read === 'no'){
-  alert('Did you escape Netflix?! I\'m practically impressed, ' + userName + '!! You should still watch Schitts Creek tho.');
+if(netflix === 'yes'){
+  alert('Yup, ' + userName + ' . I love me some Netflix!' );
+  correctAnswer ++;
+} else if(netflix === 'no'){
+  alert('Wrong, ' + userName + '! I enjoy watching Netflix.');
 } else{
-  alert(userName + '...um hello, I asked if you had a fave Netflix show!');
+  prompt('One more time ' + userName + '! Do I watch Netflix?');
 }
 
-var podcasts = prompt('Do you like podcasts?');
-var podcasts = podcasts.toUpperCase();
-var podcasts = podcasts.toLowerCase();
+//Q-3Podcast Question
+var podcasts = prompt(userName + ', do I listen to podcasts?').toLowerCase();
 // console.log(podcasts + 'this is the answer for podcasts prompt');
 if(podcasts === 'yes'){
-  alert('Me too, ' + userName + '! I like investigative and true crime podcasts the most!');
+  alert('Correct ' + userName + '! I like to listen to true crime podcasts.');
+  correctAnswer ++;
 } else if(podcasts === 'no'){
-  alert('Yeah I understand. They aren\'t for everyone, ' + userName + ' and that\'s okay! But have you tried a true crime podcast?');
+  alert('Wrong, ' + userName + '. I listen to a lot of podcasts.');
 } else{
-  alert(userName + '...um hello, I asked if you liked podcasts!');
+  prompt('One more time ' + userName + '! Do I listen to podcasts?');
 }
 
-var ferry = prompt('Have you ever ridden a ferry?');
-var ferry = ferry.toUpperCase();
-var ferry = ferry.toLowerCase();
+//Q-4Ferry Question
+var ferry = prompt(userName + ', do I like to ride ferries?').toLowerCase();
 // console.log(ferry + 'this is the answer for the ferry prompt');
 if(ferry === 'yes'){
-  alert('Me too, ' + userName + '. My favorite day trips are always ones involving ferries!');
+  alert('Yup, ' + userName + '! A ferry ride is always fun.');
+  correctAnswer ++;
 } else if (ferry === 'no'){
-  alert('Incorporating a ferry boat ride into a day trip is one of my favorite things, ' + userName + '. I highly recommend!');
+  alert('Wrong, ' + userName + '. I very much like to ride ferries.');
 } else {
-  alert(userName + '...um hello, I asked if you have ever ridden a ferry!');
+  prompt('One more time ' + userName + '! Do I like to ride ferries?');
 }
 
-var kalaloch = prompt('Have you ever heard of Kalaloch?');
-var kalaloch = kalaloch.toUpperCase();
-var kalaloch = kalaloch.toLowerCase();
+//Q-5Kalaloch Question
+var kalaloch = prompt(userName + ', do I like to visit Kalaloch?').toLowerCase();
 // console.log(kalaloch + 'this is the answer to kalaloch question');
 if(kalaloch === 'yes'){
-  alert(userName + ' I love Kalaloch! It is one of my favorite summer camping spots!');
+  alert('You guessed it, ' + userName + ' I love to visit Kalaloch.');
+  correctAnswer ++;
 } else if( kalaloch === 'no'){
-  alert('It is one of my favorite summer camping spots.  You should check it out, ' + userName + '!');
+  alert('Wrong, ' + userName + '.  I actually really like Kalaloch');
 }else {
-  alert(userName + '...um hello, I asked if you have heard of Kalaloch!');
+  prompt('One more time ' + userName + '! Do I like to visit Kalaloch?');
 }
+
+//Q-6 Numeric Question
+
+var age = prompt(userName + ', how old am I?');
+for (var i = 0; i<5; i++ ){
+  if(age < 30){
+    age = prompt(userName + ' a little too low. Guess again!');
+  } else if (age > 30){
+    age = prompt(userName + ' a little too high. Guess again!');
+  } else if (age === '30'){
+    alert(userName + ', yup! Good guess!');
+    correctAnswer ++; //this line after all correct questions is like a coin in a bucket
+    break;
+  } else {
+    alert(userName + ', you\'re out guesses! Now you\'ll never know!');
+  }
+}
+
+var faveColor = 0;
+var faveColorArray = ['orange', 'pink'];
+console.log(faveColorArray);
+while (faveColor < 6){
+  var faveColorPrompt = prompt(userName + ', what is my favorite color?');
+  for(var j = 0; j < faveColorArray.length; j++ ){ //use j because i has already been used
+    if(faveColorPrompt === faveColorArray[j]){
+      alert('correct ' + userName);
+      faveColor = 6;
+      correctAnswer ++;
+      break;
+    }else{
+      prompt(userName + ' nope not it. Guess again!');
+    }
+  }
+  faveColor ++;
+}
+
+alert(userName + 'Hey! You got it this many questions right about me: ' + correctAnswer + 'out of 7');
 
 
