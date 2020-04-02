47b3b2777ba1bb546b142a30356107acf0e0711b
Davee Sok <daveesok@gmail.com>
2020-04-01 21:06:16 -0700

    update userName function

--- js/app.js
+++ js/app.js
@@ -1,104 +1,109 @@
 'use strict';
 
-var correctAnswer = 0;
-var userName = prompt('What is your name');
-// console.log(userName + 'this is the answer for userName prompt');
-
-// Q1-Do I like to read question
-var read = prompt(userName + ', do I like to read?').toLowerCase();
-// console.log(read + 'this is answer for read prompt');
-if(read === 'yes'){
-  alert('Correct, ' + userName + '! I like to read.');
-  correctAnswer ++;
-} else if(read === 'no'){
-  alert('Wrong, ' + userName + '. I do like to read.');
-} else {
-  prompt('One more time ' + userName + '! Do I like to read?');
+function userName() {
+  var correctAnswer = 0;
+  var userName = prompt('What is your name');
+  // console.log(userName + 'this is the answer for userName prompt');
+  return userName;
 }
+var name = userName();
+console.log('This is name ' + name);
 
-// Q2-Netflex Question
-var netflix = prompt(userName + ', do I watch Netflix?').toLowerCase();
-// console.log(netflix + 'this is answer for netflix prompt');
-if(netflix === 'yes'){
-  alert('Yup, ' + userName + ' . I love me some Netflix!' );
-  correctAnswer ++;
-} else if(netflix === 'no'){
-  alert('Wrong, ' + userName + '! I enjoy watching Netflix.');
-} else{
-  prompt('One more time ' + userName + '! Do I watch Netflix?');
-}
+// // Q1-Do I like to read question
+// var read = prompt(userName + ', do I like to read?').toLowerCase();
+// // console.log(read + 'this is answer for read prompt');
+// if(read === 'yes'){
+//   alert('Correct, ' + userName + '! I like to read.');
+//   correctAnswer ++;
+// } else if(read === 'no'){
+//   alert('Wrong, ' + userName + '. I do like to read.');
+// } else {
+//   prompt('One more time ' + userName + '! Do I like to read?');
+// }
 
-//Q-3Podcast Question
-var podcasts = prompt(userName + ', do I listen to podcasts?').toLowerCase();
-// console.log(podcasts + 'this is the answer for podcasts prompt');
-if(podcasts === 'yes'){
-  alert('Correct ' + userName + '! I like to listen to true crime podcasts.');
-  correctAnswer ++;
-} else if(podcasts === 'no'){
-  alert('Wrong, ' + userName + '. I listen to a lot of podcasts.');
-} else{
-  prompt('One more time ' + userName + '! Do I listen to podcasts?');
-}
+// // Q2-Netflex Question
+// var netflix = prompt(userName + ', do I watch Netflix?').toLowerCase();
+// // console.log(netflix + 'this is answer for netflix prompt');
+// if(netflix === 'yes'){
+//   alert('Yup, ' + userName + ' . I love me some Netflix!' );
+//   correctAnswer ++;
+// } else if(netflix === 'no'){
+//   alert('Wrong, ' + userName + '! I enjoy watching Netflix.');
+// } else{
+//   prompt('One more time ' + userName + '! Do I watch Netflix?');
+// }
 
-//Q-4Ferry Question
-var ferry = prompt(userName + ', do I like to ride ferries?').toLowerCase();
-// console.log(ferry + 'this is the answer for the ferry prompt');
-if(ferry === 'yes'){
-  alert('Yup, ' + userName + '! A ferry ride is always fun.');
-  correctAnswer ++;
-} else if (ferry === 'no'){
-  alert('Wrong, ' + userName + '. I very much like to ride ferries.');
-} else {
-  prompt('One more time ' + userName + '! Do I like to ride ferries?');
-}
+// //Q-3Podcast Question
+// var podcasts = prompt(userName + ', do I listen to podcasts?').toLowerCase();
+// // console.log(podcasts + 'this is the answer for podcasts prompt');
+// if(podcasts === 'yes'){
+//   alert('Correct ' + userName + '! I like to listen to true crime podcasts.');
+//   correctAnswer ++;
+// } else if(podcasts === 'no'){
+//   alert('Wrong, ' + userName + '. I listen to a lot of podcasts.');
+// } else{
+//   prompt('One more time ' + userName + '! Do I listen to podcasts?');
+// }
 
-//Q-5Kalaloch Question
-var kalaloch = prompt(userName + ', do I like to visit Kalaloch?').toLowerCase();
-// console.log(kalaloch + 'this is the answer to kalaloch question');
-if(kalaloch === 'yes'){
-  alert('You guessed it, ' + userName + ' I love to visit Kalaloch.');
-  correctAnswer ++;
-} else if( kalaloch === 'no'){
-  alert('Wrong, ' + userName + '.  I actually really like Kalaloch');
-}else {
-  prompt('One more time ' + userName + '! Do I like to visit Kalaloch?');
-}
+// //Q-4Ferry Question
+// var ferry = prompt(userName + ', do I like to ride ferries?').toLowerCase();
+// // console.log(ferry + 'this is the answer for the ferry prompt');
+// if(ferry === 'yes'){
+//   alert('Yup, ' + userName + '! A ferry ride is always fun.');
+//   correctAnswer ++;
+// } else if (ferry === 'no'){
+//   alert('Wrong, ' + userName + '. I very much like to ride ferries.');
+// } else {
+//   prompt('One more time ' + userName + '! Do I like to ride ferries?');
+// }
 
-//Q-6 Numeric Question
+// //Q-5Kalaloch Question
+// var kalaloch = prompt(userName + ', do I like to visit Kalaloch?').toLowerCase();
+// // console.log(kalaloch + 'this is the answer to kalaloch question');
+// if(kalaloch === 'yes'){
+//   alert('You guessed it, ' + userName + ' I love to visit Kalaloch.');
+//   correctAnswer ++;
+// } else if( kalaloch === 'no'){
+//   alert('Wrong, ' + userName + '.  I actually really like Kalaloch');
+// }else {
+//   prompt('One more time ' + userName + '! Do I like to visit Kalaloch?');
+// }
 
-var age = prompt(userName + ', how old am I?');
-for (var i = 0; i<5; i++ ){
-  if(age < 30){
-    age = prompt(userName + ' a little too low. Guess again!');
-  } else if (age > 30){
-    age = prompt(userName + ' a little too high. Guess again!');
-  } else if (age === '30'){
-    alert(userName + ', yup! Good guess!');
-    correctAnswer ++; //this line after all correct questions is like a coin in a bucket
-    break;
-  } else {
-    alert(userName + ', you\'re out guesses! Now you\'ll never know!');
-  }
-}
+// //Q-6 Numeric Question
 
-var faveColor = 0;
-var faveColorArray = ['orange', 'pink'];
-console.log(faveColorArray);
-while (faveColor < 6){
-  var faveColorPrompt = prompt(userName + ', what is my favorite color?');
-  for(var j = 0; j < faveColorArray.length; j++ ){ //use j because i has already been used
-    if(faveColorPrompt === faveColorArray[j]){
-      alert('correct ' + userName);
-      faveColor = 6;
-      correctAnswer ++;
-      break;
-    }else{
-      prompt(userName + ' nope not it. Guess again!');
-    }
-  }
-  faveColor ++;
-}
+// var age = prompt(userName + ', how old am I?');
+// for (var i = 0; i<5; i++ ){
+//   if(age < 30){
+//     age = prompt(userName + ' a little too low. Guess again!');
+//   } else if (age > 30){
+//     age = prompt(userName + ' a little too high. Guess again!');
+//   } else if (age === '30'){
+//     alert(userName + ', yup! Good guess!');
+//     correctAnswer ++; //this line after all correct questions is like a coin in a bucket
+//     break;
+//   } else {
+//     alert(userName + ', you\'re out guesses! Now you\'ll never know!');
+//   }
+// }
 
-alert(userName + 'Hey! You got it this many questions right about me: ' + correctAnswer + 'out of 7');
+// var faveColor = 0;
+// var faveColorArray = ['orange', 'pink'];
+// console.log(faveColorArray);
+// while (faveColor < 6){
+//   var faveColorPrompt = prompt(userName + ', what is my favorite color?');
+//   for(var j = 0; j < faveColorArray.length; j++ ){ //use j because i has already been used
+//     if(faveColorPrompt === faveColorArray[j]){
+//       alert('correct ' + userName);
+//       faveColor = 6;
+//       correctAnswer ++;
+//       break;
+//     }else{
+//       prompt(userName + ' nope not it. Guess again!');
+//     }
+//   }
+//   faveColor ++;
+// }
+
+// alert(userName + 'Hey! You got it this many questions right about me: ' + correctAnswer + 'out of 7');
 
 
